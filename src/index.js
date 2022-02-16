780113ba96a772518544a4da94ecf3e5745ad44d
Natallia Bakinouskaya <natalliabakinouskaya@Natallias-MacBook-Air.local>
2022-02-16 21:24:05 +0200

    initial: ready

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports =
+    function towelSort(matrix) {
+        let res = [];
+        if (!matrix) {
+            res = [];
+        } else {
+            for (let i = 1; i < matrix.length; i += 2) {
+                matrix[i].reverse()
+            }
+            for (let i = 0; i < matrix.length; i++) {
+                for (let j = 0; j < matrix[i].length; j++) {
+
+                    res.push(matrix[i][j])
+                }
+            }
+        }
+
+        return res;
+    }
+    // console.log(towelSort([]))
